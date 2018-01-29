10d2aae5d08b1b67d8bfb8bd7e276678e95303a1
nivred <emaildervin@gmail.com>
2018-01-28 22:58:06 -0600

    public, views, models, and server code added

---
+++ models/Article.js
@@ -0,0 +1,30 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var ArticleSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  link: {
+    type: String,
+    required: true
+  },
+  blurb: {
+    type: String,
+    required: true
+  },
+  note: {
+    type: Schema.Types.ObjectId,
+    ref: "Note"
+  },
+  saved:{
+    type: Boolean,
+    default: false,
+  }
+});
+
+var Article = mongoose.model("Article", ArticleSchema);
+
+module.exports = Article;
